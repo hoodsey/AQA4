c152af21e1ec64229857815315c437f927f59be5
Nasya Kabakova <kabakova.nas@gmail.com>
2023-11-01 23:22:36 +0300

    add new tests

--- src/test/java/ru/netology/BankCardDeliveryTest.java
+++ src/test/java/ru/netology/BankCardDeliveryTest.java
@@ -5,71 +5,120 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class BankCardDeliveryTest {
-    private static WebDriver driver;
-
-
     public String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    @BeforeAll
-    public static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
 
     @BeforeEach
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        open("http://localhost:9999");
+        $("[data-test-id='date'] [placeholder='Дата встречи']").sendKeys(Keys.CONTROL,"a" + Keys.DELETE);
+
     }
 
-    @AfterEach
-    void tearDown(){
-        driver.quit();
-        driver = null;
-    }
 
     @Test
     void ShouldSuccessForm(){
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Краснодар");
-        $("[data-test-id='date'] input").setValue(generateDate(8));
+        String date =  generateDate(8);
+        $("[data-test-id='date'] input").val(generateDate(8));
         $("[data-test-id='name'] input").setValue("Кабакова Анастасия");
         $("[data-test-id='phone'] input").setValue("+79818042544");
         $("[data-test-id='agreement']").click();
         $(".button").click();
         $(".notification__content").shouldBe(visible, Duration.ofSeconds(15));
-        $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + generateDate(3)));
+        $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + generateDate(8)));
     }
     @Test
     void lastDateTest(){
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Краснодар");
         $("[data-test-id='date'] input").setValue(generateDate(-2));
         $("[data-test-id='name'] input").setValue("Кабакова Анастасия");
         $("[data-test-id='phone'] input").setValue("+79818042544");
         $("[data-test-id='agreement']").click();
         $(".button").click();
-        //$(".notification__content").shouldBe(visible, Duration.ofSeconds(15));
-        //$(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + generateDate(3)));
+        $("[data-test-id='date'] [class='input__sub']").shouldBe(ownText("Заказ на выбранную дату невозможен"));
+
     }
+    @Test
+    void CityWithOutCatalog(){
+        $("[data-test-id='city'] input").setValue("Канадлакша");
+        $("[data-test-id='date'] input").setValue(generateDate(-2));
+        $("[data-test-id='name'] input").setValue("Кабакова Анастасия");
+        $("[data-test-id='phone'] input").setValue("+79818042544");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='city'] [class='input__sub']").shouldHave(ownText("Доставка в выбранный город недоступна"));
+    }
+    @Test
+    void IncorrectName(){
+        $("[data-test-id='city'] input").setValue("Краснодар");
+        $("[data-test-id='date'] input").setValue(generateDate(4));
+        $("[data-test-id='name'] input").setValue("Anastasiia 123");
+        $("[data-test-id='phone'] input").setValue("+79818042544");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='name'] [class='input__sub']").shouldHave(ownText("Имя и Фамилия указаные неверно."));
+    }
+    @Test
+    void IncorrectPhone(){
+        $("[data-test-id='city'] input").setValue("Краснодар");
+        $("[data-test-id='date'] input").setValue(generateDate(4));
+        $("[data-test-id='name'] input").setValue("Кабакова Анастасия");
+        $("[data-test-id='phone'] input").setValue("98818042544");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='phone'] [class='input__sub']").shouldHave(ownText("Телефон указан неверно."));
+    }
+    @Test
+    void WithOutAgree(){
+        $("[data-test-id='city'] input").setValue("Краснодар");
+        $("[data-test-id='date'] input").setValue(generateDate(4));
+        $("[data-test-id='name'] input").setValue("Кабакова Анастасия");
+        $("[data-test-id='phone'] input").setValue("+79818042544");
+        $(".button").click();
+        $("[data-test-id='agreement'] [class='checkbox__text']").shouldHave(ownText("Я соглашаюсь с условиями обработки"));
+    }
+
+    @Test
+    void shouldEmptyCity() {
+        $("[data-test-id='date'] input").setValue(generateDate(4));
+        $("[data-test-id='name'] input").setValue("Кабакова Анастасия");
+        $("[data-test-id='phone'] input").setValue("+79818042544");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='city'] [class='input__sub']").shouldHave(ownText("Поле обязательно для заполнения"));
+    }
+    @Test
+    void shouldEmptyName() {
+        $("[data-test-id='date'] input").setValue(generateDate(4));
+        $("[data-test-id='city'] input").setValue("Краснодар");
+        $("[data-test-id='phone'] input").setValue("+79818042544");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='name'] [class='input__sub']").shouldHave(ownText("Поле обязательно для заполнения"));
+    }
+    @Test
+    void shouldEmptyPhone() {
+        $("[data-test-id='date'] input").setValue(generateDate(4));
+        $("[data-test-id='city'] input").setValue("Краснодар");
+        $("[data-test-id='name'] input").setValue("Кабакова Анастасия");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='phone'] [class='input__sub']").shouldHave(ownText("Поле обязательно для заполнения"));
+    }
+
 }
